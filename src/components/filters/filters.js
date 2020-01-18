9bec99d90090c7830d75ce5fac4d990c12bbc67c
EvNomad <b.boy.bip@gmail.com>
2020-01-18 10:29:33 +0700

    38 Udemy

--- src/components/filters/filters.js
+++ src/components/filters/filters.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import "./filters.css";
 
-const Filters = () => {
-  return (
-    <div className="filters">
-      <button type="button" className="btn btn-info">All</button>
-      <button type="button" className="btn btn-outline-secondary">Active</button>
-      <button type="button" className="btn btn-outline-secondary">Done</button>
-    </div>
-  );
-};
 
-export default Filters;
+
+export default class Filters extends Component {
+	render() {
+		return (
+			<div className="filters">
+				<button type="button" className="btn btn-info">All</button>
+				<button type="button" className="btn btn-outline-secondary">Active</button>
+				<button type="button" className="btn btn-outline-secondary">Done</button>
+			</div>
+		);
+	}
+}
+
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import "./todo-list-item.css";
 
-const TodoListItem = ( { label, important = false } ) => {
-	
-	const style = {
-		color: important ? 'darkslateblue' : 'black',
-		fontWeight: important ? 'bold' : 'normal',
-		fontSize: '16px'
-	};
-	
-	return (
-		<span 
-			className="todo-list-item" 
-			style={ style }>
-			{ label }
-			<button type="button" class="btn btn-outline-success"><i class="fa fa-exclamation"></i></button>
-			<button type="button" class="btn btn-outline-danger"><i class="fa fa-trash-o"></i></button>
-		</span>
-	);
-};
+export default class TodoListItem extends Component {
+  state = {
+    done: false,
+    important: false
+  };
 
+  onLabelClick = () => {
+    this.setState( ({ done }) => {
+			return {
+				done: !done
+			}
+		});
+  };
 
-export default TodoListItem;
+  onImportantClick = () => {
+    this.setState(( { important }) => {
+			return {
+				important: !important
+			}
+		});
+  };
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = "todo-item-label";
+    if (done) classNames += " done";
+    if (important) classNames += " important";
+
+    return (
+      <span className="todo-list-item">
+        <span className={classNames} onClick={this.onLabelClick}>
+          {label}
+        </span>
+        <button
+          type="button"
+          onClick={this.onImportantClick}
+          className="btn btn-outline-success"
+        >
+          <i className="fa fa-exclamation"></i>
+        </button>
+        <button type="button" className="btn btn-outline-danger">
+          <i className="fa fa-trash-o"></i>
+        </button>
+      </span>
+    );
+  }
+}
